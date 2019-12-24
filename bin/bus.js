2b425055b8da0513918b92723195fa3c3ad9093b
xuexingwei <xuexingwei@bus365.com>
2019-12-24 17:28:37 +0800

    新增jquery工程模板

--- bin/bus.js
+++ bin/bus.js
@@ -9,33 +9,46 @@ const inquirer = require('inquirer');
 
 
 program
-    .version('1.0.3')
+    .version('1.0.4')
     .description('Bus365 mobile application development vue project scaffolding')
 program
     .command('init <project>')
     .action(function(tpl, project) {
         inquirer.prompt([{
-            type: 'confirm',
-            name: 'ISSPA',
-            message: '创建单页应用?',
+            type: 'list',
+            name: 'proType',
+            message: '选择项目类型',
+            choices: [
+                { name: 'vueTpl', value: 1 },
+                { name: 'vueMtpl', value: 2 },
+                { name: 'jquery', value: 3 }
+            ],
             default: true
         }]).then((answers) => {
-            console.log(answers.ISSPA)
-            if (answers.ISSPA) {
+            console.log(answers.proType)
+            if (answers.proType === 1) {
                 log.info(`正在拉取单页模板代码，下载位置：${process.cwd()}/ ...`)
                 console.log(process.cwd())
                 clone(`http://192.168.3.46/phone/vueTpl.git`, `${process.cwd()}`, null, function() {
                     // shell.rm('-rf', pwd + `/fruit/.git`)
                     deleteFolder(`${process.cwd()}/.git`);
-                    log.info('模板工程建立完成')
+                    log.info('单页模板工程建立完成')
                 })
-            } else {
+            } else if(answers.proType === 2) {
                 log.info(`正在拉取多页模板代码，下载位置：${process.cwd()}/ ...`)
                 console.log(process.cwd())
                 clone(`http://192.168.3.46/phone/vueMtpl.git`, `${process.cwd()}`, null, function() {
                     // shell.rm('-rf', pwd + `/fruit/.git`)
                     deleteFolder(`${process.cwd()}/.git`);
-                    log.info('模板工程建立完成')
+                    log.info('多页模板工程建立完成')
+                })
+            }else{
+                log.info(`正在拉取jquery模板代码，下载位置：${process.cwd()}/ ...`)
+                console.log(process.cwd())
+                clone(`http://192.168.3.46/phone/jqTpl.git`, `${process.cwd()}`, null, function() {
+                    // shell.rm('-rf', pwd + `/fruit/.git`)
+                    deleteFolder(`${process.cwd()}/.git`);
+                    log.info('jquery模板工程建立完成')
                 })
             }
 
